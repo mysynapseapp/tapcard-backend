ceb4833394c97261d6dfdd5f657c75ef7c90ea05
Masaru124 <masarukaze041@gmail.com>
2025-08-09 11:20:12 +0530

    db connection

--- database.py
+++ database.py
@@ -9,6 +9,8 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+elif DATABASE_URL.startswith("postgresql"):
+    engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 else:
     engine = create_engine(DATABASE_URL)
 
